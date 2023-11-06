d53a29225b7a52e2ce89a1b7ebba114600c1b42f
vitamingyu <codemin9@gmail.com>
2023-11-06 18:06:39 +0900

    프로퍼티스

--- src/main/java/pack/Broccoli123Application.java
+++ src/main/java/pack/Broccoli123Application.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import pack.customer.RecaptchaConfig;
 
 @SpringBootApplication
-public class Broccoli123Application implements CommandLineRunner {
+public class Broccoli123Application extends SpringBootServletInitializer implements CommandLineRunner {
 
     @Autowired
     private RecaptchaConfig recaptchaConfig;
@@ -24,7 +26,10 @@ public class Broccoli123Application implements CommandLineRunner {
     public void run(String... args) throws Exception {
         // 애플리케이션이 시작될 때 Google reCAPTCHA 비밀 키 설정
         recaptchaConfig.setSecretKey(recaptchaSecretKey);
+    }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Broccoli123Application.class);
     }
 }
