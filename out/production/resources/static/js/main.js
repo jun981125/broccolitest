ae8e9c7e48c7c4f078e3f541dfeaaff1150ab7ee
vitamingyu <codemin9@gmail.com>
2023-12-07 12:50:26 +0900

    무한스크롤 이미지 갯수 살짝 줄임

--- out/production/resources/static/js/main.js
+++ out/production/resources/static/js/main.js
@@ -1,5 +1,7 @@
 // 메인 슬라이더 설정
 var currentPage = 2;
+var totalLoadedProducts = 0; // 로드된 상품의 총 개수
+
 $(document).ready(function () {
     $('.flexslider').flexslider({
         animation: 'slide',
@@ -32,27 +34,31 @@ function productLoad() {
             // 받은 데이터를 이용하여 동적으로 HTML 생성
             var html = '';
             response.list.forEach(function(product) {
-                html += '<div class="list_con">' +
-                    '    <div class="img_container">';
+                if (totalLoadedProducts < 24) { // 32개까지만 로드
+                    html += '<div class="list_con">' +
+                        '    <div class="img_container">';
 
-                if (product.pimage != null) {
-                    // 이미지가 있는 경우
-                    html += '        <a href="/showproduct?productid=' + product.productid + '&page=' + response.currentPage + '">';
-                    html += '            <img src="/upload/' + product.pimage + '" />';
-                    html += '        </a>';
-                } else {
-                    // 이미지가 없는 경우
-                    html += '        <img src="/images/default-image.jpg" alt="No Image" />';
+                    if (product.pimage != null) {
+                        // 이미지가 있는 경우
+                        html += '        <a href="/showproduct?productid=' + product.productid + '&page=' + response.currentPage + '">';
+                        html += '            <img src="/upload/' + product.pimage + '" />';
+                        html += '        </a>';
+                    } else {
+                        // 이미지가 없는 경우
+                        html += '        <img src="/images/default-image.jpg" alt="No Image" />';
+                    }
+                    html += '    </div>' +
+                        '<div class="text_container">' +
+                        '<h3>' +
+                        '    <span class="brd_name">' + product.brand + '</span>' +
+                        '    <h3 class="prd_name">' + product.model + '</h3>' +
+                        '</h3>' +
+                        '</div>' +
+                        '    <p class="price">' + new Intl.NumberFormat('ko-KR').format(product.price) + '원</p>' +
+                        '</div>';
+
+                    totalLoadedProducts++;
                 }
-                html += '    </div>' +
-                    '<div class="text_container">' +
-                    '<h3>' +
-                    '    <span class="brd_name">' + product.brand + '</span>' +
-                    '    <h3 class="prd_name">' + product.model + '</h3>' +
-                    '</h3>' +
-                    '</div>' +
-                    '    <p class="price">' + new Intl.NumberFormat('ko-KR').format(product.price) + '원</p>' +
-                    '</div>';
             });
 
             // 생성된 HTML을 #endList 전에 추가
@@ -61,7 +67,7 @@ function productLoad() {
             // 로딩 중 표시 숨김
             $(".loading-indicator").hide();
             // 받은 데이터를 JSON 문자열로 변환하여 출력
-            currentPage +=1;
+            currentPage += 1;
         },
 
         error: function () {
@@ -80,3 +86,11 @@ function callback(entries, observer) {
         }
     });
 }
+
+// 스크롤 이벤트에 대한 처리
+$(window).scroll(function() {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        // 스크롤이 맨 아래에 도달했을 때 추가 상품 로딩
+        productLoad();
+    }
+});
